236e21acb14806610e8ed3a4b5abf67889a26ac0
Andrew MacGillivray <agjmacgill@gmail.com>
2022-05-04 15:35:20 -0500

    Finished createpostshandler.php

--- createpostshandler.php
+++ createpostshandler.php
@@ -9,8 +9,6 @@
 
 require_once("./common.php");
 
-var_dump($_POST);
-
 ## 
 # GETTING POST ARGUMENTS
 ##
@@ -20,7 +18,6 @@ $vp = ((isset($_POST["post"])) && (strlen($_POST["post"]) > 0)); # Valid post
 $user = ($vu) ? $_POST["user"] : "";
 $post = ($vp) ? $_POST["post"] : "";
 # Update valid user bool - user value must match a record in db
-if ($vu) $vu = user_exists($user);
 $v = ($vp && $vu); # Valid args?
 
 ##
@@ -29,19 +26,20 @@ $v = ($vp && $vu); # Valid args?
 html_open("Handling Post Creation");
 if ($v)
 {
+    $post = base64_encode($post);
     print "<h1>Creating post for $user</h1>";
     # Create the post
     if (create_post( $user, $post ))
     {
-        
-    }
+        print "<p>Post added successfully.</p>";
+    } else {
+        print "<p>Error: Provided username '$user' does not match any existing user.</p>";        
+    }  
 } else {
     # Indicate any issues with the vu / vp values 
     if (!$vu) 
-        print "<p>Error: Provided username was empty or did not match any existing user.</p>";
+        print "<p>Error: Provided username was empty or non-interpretable</p>";
     if (!$vp) 
         print "<p>Error: Provided post text is empty</p>";
 }
 html_close();
- 
-// var_dump($_POST);
